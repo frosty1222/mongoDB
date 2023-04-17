7bd05567a22df97c6c6e5bb5a7656af3e20dd6db
Duong Tran <duongtd@aht-asia.com>
2023-04-17 12:36:48 +0700

    install mongodb compass

--- dbconnect.js
+++ dbconnect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 async function connect(){
      try{
-     await mongoose.connect('mongodb://127.0.0.1:27017/resfulApi',{
+     await mongoose.connect('mongodb://127.0.0.1:27017/angularRestApi',{
          useNewUrlParser: true,
          useUnifiedTopology:true
      })
