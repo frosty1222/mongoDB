0834ae1421bf7a75cf1c0b472b86de6585bedfd0
Trần Đại Dương(Asia) <duongtd@aht-asia.com>
2023-04-26 21:46:51 +0700

    no messsage

--- routes/users.js
+++ routes/users.js
@@ -5,4 +5,5 @@ router.get('/user-index',user_controller.index);
 router.post('/user-add',user_controller.addUser);
 router.get('/user-by-id/:id',user_controller.findOne);
 router.put('/user-update/:id',user_controller.updateUser);
+router.delete('/user-delete/:id',user_controller.deleteUser)
 module.exports = router;
