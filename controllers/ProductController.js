31342e8e3c5e09db47788ab6d4e957224f37f439
donglv <donglv@aht-asia.com>
2023-06-17 17:16:04 +0700

    no message

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,18 +1,54 @@
+const product  = require('../models/Product');
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, '../public/uploads/'); // Destination folder for uploaded images
+    },
+    filename: (req, file, cb) => {
+      const fileName = `${file.originalname}`;
+      cb(null, fileName);
+    }
+  });
 class ProductController{
-    index(req,res){
-
+    async index(req,res){
+        const Product = await product();
     }
-    addProduct(req,res){
-
-    }
-    deleteProduct(req,res){
-
-    }
-    editProduct(req,res){
-
-    }
-    getProById(req,res){
+    async addProduct(req,res){
+        try {
+            upload(req, res, async function (err) {
+              if (err instanceof multer.MulterError) {
+                return res.status(400).json({ error: 'Error uploading file' });
+              } else if (err) {
+                return res.status(500).json({ error: 'Internal server error' });
+              }
+              
+              const Product = await product();
+              const imageUrl = `uploads/${req.file.filename}`;
         
+              Product.create({ imageUrl })
+                .then(createdImage => {
+                console.log("createdImage",createdImage)
+                  res.json({
+                    message: "Upload successful",
+                    success: true,
+                  });
+                })
+                .catch(err => {
+                  res.status(500).json({ error: err.message });
+                });
+            });
+          } catch (err) {
+            res.status(500).json({ error: err.message });
+          }
+    }
+    async deleteProduct(req,res){
+        const Product = await product();
+    }
+    async editProduct(req,res){
+        const Product = await product();
+    }
+    async getProById(req,res){
+        const Product = await product();
     }
 }
 module.exports = new ProductController();
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 const testRouter = require('./tests');
 const userRouter = require('./users');
 const roleRouter = require('./roles');
+const productRouter = require('./product');
+const perRouter = require('./permission');
+const desRouter = require('./product_description');
+const rolehasperRouter = require('./role_has_per');
 function route(app) {
     app.use('/test',testRouter);
     app.use('/user',userRouter);
     app.use('/role',roleRouter);
+    app.use('/product',productRouter);
+    app.use('/permission',perRouter);
+    app.use('/description',desRouter);
+    app.use('/rolehasper',rolehasperRouter);
 }
 module.exports = route;
